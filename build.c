50e7fcfe451d91237dcfb7028d30dcdc9e2a1e8a
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-08 13:23:08 +0100

    buffer: example of ByteAddressBuffer

--- build.c
+++ build.c
@@ -6,6 +6,7 @@ int main() {
   if (0 != _spawnlp(_P_WAIT, "clang.exe", "clang.exe", "-o", "cbuffer.exe", "cbuffer.c", NULL)) return 1;
   if (0 != _spawnlp(_P_WAIT, "clang.exe", "clang.exe", "-o", "cbuffer-desc.exe", "cbuffer-desc.c", NULL)) return 1;
   if (0 != _spawnlp(_P_WAIT, "clang.exe", "clang.exe", "-o", "cbuffer-root.exe", "cbuffer-root.c", NULL)) return 1;
+  if (0 != _spawnlp(_P_WAIT, "clang.exe", "clang.exe", "-o", "buffer.exe", "buffer.c", NULL)) return 1;
 
   return 0;
 }
